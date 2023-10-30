2e7f4f4e9a33d7d4d2f78e07859e8a81d9a63dc1
Sapito Sucio <7357354+SapitoSucio@users.noreply.github.com>
2023-10-30 13:45:55 -0600

    Transición Suave de blanco a negro y viceversa

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     darkModeButton.addEventListener('click', function () {
         document.body.classList.toggle('dark-mode');
+        document.body.classList.toggle('light-mode');
     });
 
     function calculateBitmask() {
